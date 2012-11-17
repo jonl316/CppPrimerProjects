f11a1b8c7fd5822f328c4ef3d9857cffc4f4b2ba
Jonathan David Livesay <jonl316@gmail.com>
2012-11-17 15:21:58 -0600

    Initial Commit

---
+++ CppPrimerProjects/Ch1/Working.cpp
@@ -0,0 +1,21 @@
+/*
+ * Working.cpp
+ *
+ *  Created on: Nov 17, 2012
+ *      Author: Jonathan David Livesay
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+
---
+++ Working/Debug/Working.d
@@ -0,0 +1,1 @@
+Working.d: ../Working.cpp
